a3ee1736f4dee581f84eb25f375e2f3282e3a34b
abidalili <abidalili1995@gmail.com>
2023-03-10 00:21:57 +0200

    js error handling

---
+++ js error handling- try, catch, finally block/index.js
@@ -0,0 +1,17 @@
+// error handling -> try, catch, finally,throw
+// try..... catch handle run time errors(program which is )
+//Error object
+//finally statement -> execute code, after try and catch
+
+try {
+  //code test
+  alert("hi everyone");
+  alert(x);
+  alert("im abida");
+  alert("bye everyone");
+} catch (err) {
+  // console.log(err);
+  alert("inside catch block");
+} finally {
+  alert("bye everyone");
+}
