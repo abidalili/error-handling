e4bb4f8e1e62d576b04269f0a1b7c662048808e2
abidalili <abidalili1995@gmail.com>
2023-03-10 01:06:02 +0200

    throw error handling

---
+++ js error handling- throw keyword(part-2)/index.js
@@ -0,0 +1,15 @@
+// Error handling . try,catch,finally
+// The throw statement -> create custom errors.
+
+document.querySelector("#checkButton").addEventListener("click", function () {
+  var num = document.querySelector("#numTextField").value;
+  try {
+    if (num < 5) {
+      throw "input is too low";
+    } else if (num > 10) {
+      throw "input is too high";
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
